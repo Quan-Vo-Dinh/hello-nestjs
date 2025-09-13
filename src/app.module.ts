cdd42bfafc30bf52a1d8c4d04e4c6f0ed062dc4a
Quan-Vo-Dinh <vodinhquan2707.it@gmail.com>
2025-09-13 15:21:15 +0700

    feat(setup): initialize prisma with sqlite and create global shared module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { SharedModule } from './shared/shared.module'
+import { PostsModule } from './routes/posts/posts.module'
 
 @Module({
-  imports: [],
+  imports: [SharedModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
